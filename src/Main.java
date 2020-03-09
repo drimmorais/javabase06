bf834ba25da9cc5aa1f8c67a21a56853d99849a2
Matuk <mateusmatuk@fai-mg.br>
2020-03-09 19:47:46 -0300

    finxinho

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,39 @@
 
+import java.util.Scanner;
+
 public class Main {
 
+	public static void main(String[] args) {
+
+    int i;
+
+    String[] carros = new String [5];
+
+    carros[1] = "Civic";
+    carros[2] = "Gol";
+    carros[3] = "Palio";
+    carros[4] = "Uno";
+
+  
+    for(i = 1; i < 5; i++){
+        System.out.println(carros[i]);
+    }
+
+    carros[2] = "L200";
+
+    for(i = 1; i < 5; i++){
+        System.out.println(carros[i]);
+    }
+
+    carros[1] = "Civic";
+    carros[2] = "Palio";
+    carros[3] = "Uno";    
+    carros[4] = "Gol";
+
+     for(i = 1; i < 5; i++){
+        System.out.println(carros[i]);
+    }
+    
+    
+  }
 }
